e8388aa7ad793f3bc4185b9d2561d6265dbd1b63
Andrey Popp <8mayday@gmail.com>
2017-03-28 18:49:19 +0300

    Reformat code with prettier

--- index.js
+++ index.js
@@ -17,49 +17,54 @@
 
 /* jshint node: true, strict: true */
 
-    "use strict";
+'use strict';
 
-    module.exports.substiteVariables = substiteVariables;
+module.exports.substiteVariables = substiteVariables;
 
-    function substiteVariable(variable, options, cb) {
-        var value;
-        var err = null;
-        var s = variable.split(':', 2);
-        if (s.length == 2) {
-            value = options.env[s[0]];
-            if (typeof value == 'function') {
-                value = value();
-            }
-            if (s[1][0] == '+') { // Substitute replacement, but only if variable is defined and nonempty. Otherwise, substitute nothing
-                value = value ? s[1].substring(1) : '';
-            } else if (s[1][0] == '-') { // Substitute the value of variable, but if that is empty or undefined, use default instead
-                value = value || s[1].substring(1);
-            } else if (s[1][0] == '#') { // Substitute with the length of the value of the variable
-                value = value !== undefined ? String(value).length : 0;
-            } else if (s[1][0] == '=') { // Substitute the value of variable, but if that is empty or undefined, use default instead and set the variable to default
-                if (!value) {
-                    value = s[1].substring(1);
-                    options.env[s[0]] = value;
-                }
-            } else if (s[1][0] == '?') { // If variable is defined and not empty, substitute its value. Otherwise, print message as an error message.
-                if (!value) {
-                    if (s[1].length > 1) {
-                        return cb(s[0] + ': ' + s[1].substring(1));
-                    } else {
-                        return cb(s[0] + ': parameter null or not set');
-                    }
-                }
-            }
-        } else {
-            value = options.env[variable];
-            if (typeof value == 'function') {
-                value = value();
-            }
-        }
-        cb(err, value);
+function substiteVariable(variable, options, cb) {
+  var value;
+  var err = null;
+  var s = variable.split(':', 2);
+  if (s.length == 2) {
+    value = options.env[s[0]];
+    if (typeof value == 'function') {
+      value = value();
     }
+    if (s[1][0] == '+') {
+      // Substitute replacement, but only if variable is defined and nonempty. Otherwise, substitute nothing
+      value = value ? s[1].substring(1) : '';
+    } else if (s[1][0] == '-') {
+      // Substitute the value of variable, but if that is empty or undefined, use default instead
+      value = value || s[1].substring(1);
+    } else if (s[1][0] == '#') {
+      // Substitute with the length of the value of the variable
+      value = value !== undefined ? String(value).length : 0;
+    } else if (s[1][0] == '=') {
+      // Substitute the value of variable, but if that is empty or undefined, use default instead and set the variable to default
+      if (!value) {
+        value = s[1].substring(1);
+        options.env[s[0]] = value;
+      }
+    } else if (s[1][0] == '?') {
+      // If variable is defined and not empty, substitute its value. Otherwise, print message as an error message.
+      if (!value) {
+        if (s[1].length > 1) {
+          return cb(s[0] + ': ' + s[1].substring(1));
+        } else {
+          return cb(s[0] + ': parameter null or not set');
+        }
+      }
+    }
+  } else {
+    value = options.env[variable];
+    if (typeof value == 'function') {
+      value = value();
+    }
+  }
+  cb(err, value);
+}
 
-    /**
+/**
      * Substitute all the occurrences of environ variables in a text
      *
      * @param {String} text - Text with variables to be substituted
@@ -67,90 +72,99 @@
      * @param {String|array} options.specialVars - List of special (single char) variables
      * @param {function} cb - Callback function
      */
-    function substiteVariables(text, options, cb) {
-        options = options || {};
-        if (!options.env) {
-            options.env = {};
-        }
-        return substiteVariablesInternal(text, 0, '', options, cb);
-    }
+function substiteVariables(text, options, cb) {
+  options = options || {};
+  if (!options.env) {
+    options.env = {};
+  }
+  return substiteVariablesInternal(text, 0, '', options, cb);
+}
 
-    function substiteVariablesInternal(str, position, result, options, cb) {
-        if (position == -1 || !str) {
-            cb(null, result);
+function substiteVariablesInternal(str, position, result, options, cb) {
+  if (position == -1 || !str) {
+    cb(null, result);
+  } else {
+    var index = str.indexOf('$', position);
 
+    if (index == -1) {
+      // no $
+      result += str.substring(position);
+      position = -1;
+      cb(null, result);
+    } else {
+      // $ found
+      var variable;
+      var endIndex;
+      result += str.substring(position, index);
+
+      if (str.charAt(index + 1) == '{') {
+        // ${VAR}
+        endIndex = str.indexOf('}', index);
+        if (endIndex == -1) {
+          // '}' not found
+          if (options.ignoreErrors) {
+            variable = str.substring(index + 2);
+          } else {
+            return cb('unexpected EOF while looking for matching }');
+          }
         } else {
-            var index = str.indexOf('$', position);
-
-            if (index == -1) { // no $
-                result += str.substring(position);
-                position = -1;
-                cb(null, result);
-
-            } else { // $ found
-                var variable;
-                var endIndex;
-                result += str.substring(position, index);
-
-                if (str.charAt(index+1) == '{') { // ${VAR}
-                    endIndex = str.indexOf('}', index);
-                    if (endIndex == -1) { // '}' not found
-                        if (options.ignoreErrors) {
-                            variable = str.substring(index+2);
-                        } else {
-                            return cb('unexpected EOF while looking for matching }');
-                        }
-                    } else { // '}' found
-                        variable = str.substring(index+2, endIndex);
-                        endIndex++;
-                    }
-                    if (!variable) {
-                        result += '${}';
-                    }
-                } else { // $VAR
-                    index++; // skip $
-                    endIndex = -1;
-                    // special single char vars
-                    if (options.specialVars && options.specialVars.indexOf(str[index]) != -1) {
-                        variable = str[index];
-                        endIndex = index + 1;
-                    } else {
-                        // search for var end
-                        for (var i = index, len = str.length; i < len; i++) {
-                            var code = str.charCodeAt(i);
-                            if (!(code > 47 && code < 58) &&  // numeric
-                                !(code > 64 && code < 91) &&  // upper alpha
-                                (code !== 95) &&              // underscore
-                                !(code > 96 && code < 123)) { // lower alpha
-                                endIndex = i;
-                                break;
-                            }
-                        }
-
-                        if (endIndex == -1) { // delimeter not found
-                            variable = str.substring(index);
-                        } else { // delimeted found
-                            variable = str.substring(index, endIndex);
-                        }
-                    }
-                    if (!variable) {
-                        result += '$';
-                    }
-                }
-                position = endIndex;
-                if (! variable) {
-                    substiteVariablesInternal(str, position, result, options, cb);
-                } else {
-                    substiteVariable(variable, options,  function callback(err, value) {
-                        if (err && !options.ignoreErrors) {
-                            return cb(err);
-                        }
-                        if (value !== null && value !== undefined) {
-                            result += String(value);
-                        }
-                        substiteVariablesInternal(str, position, result, options, cb);
-                    });
-                }
-            }
+          // '}' found
+          variable = str.substring(index + 2, endIndex);
+          endIndex++;
         }
+        if (!variable) {
+          result += '${}';
+        }
+      } else {
+        // $VAR
+        index++; // skip $
+        endIndex = -1;
+        // special single char vars
+        if (options.specialVars && options.specialVars.indexOf(str[index]) != -1) {
+          variable = str[index];
+          endIndex = index + 1;
+        } else {
+          // search for var end
+          for (var i = index, len = str.length; i < len; i++) {
+            var code = str.charCodeAt(i);
+            if (
+              !(code > 47 && code < 58) && // numeric
+              !(code > 64 && code < 91) && // upper alpha
+              code !== 95 && // underscore
+              !(code > 96 && code < 123)
+            ) {
+              // lower alpha
+              endIndex = i;
+              break;
+            }
+          }
+
+          if (endIndex == -1) {
+            // delimeter not found
+            variable = str.substring(index);
+          } else {
+            // delimeted found
+            variable = str.substring(index, endIndex);
+          }
+        }
+        if (!variable) {
+          result += '$';
+        }
+      }
+      position = endIndex;
+      if (!variable) {
+        substiteVariablesInternal(str, position, result, options, cb);
+      } else {
+        substiteVariable(variable, options, function callback(err, value) {
+          if (err && !options.ignoreErrors) {
+            return cb(err);
+          }
+          if (value !== null && value !== undefined) {
+            result += String(value);
+          }
+          substiteVariablesInternal(str, position, result, options, cb);
+        });
+      }
     }
+  }
+}
--- test/test.js
+++ test/test.js
@@ -21,149 +21,166 @@ var should = require('should');
 var varExpansion = require('../');
 var substiteVariables = varExpansion.substiteVariables;
 
-describe('Variables substitution',function(){
-    "use strict";
+describe('Variables substitution', function() {
+  'use strict';
+  var line0 = 'la vispa ${name} altro testo $BOH noo$questoXX prova${A}aa';
+  var env = {
+    name: 'ciccio',
+    questoXX: 'aahhh',
+    BOH: 1,
+    PWD: '/Users/andreax',
+    '?': 0,
+    f: function() {
+      return 295;
+    },
+    $: 12345,
+  };
 
-    var line0 = "la vispa ${name} altro testo $BOH noo$questoXX prova${A}aa";
-    var env =  { name: 'ciccio', questoXX: 'aahhh', BOH: 1, PWD: '/Users/andreax', '?': 0, f: function() { return 295; }, $: 12345 }; 
+  it('should accept empty environ', function(done) {
+    substiteVariables(line0, {}, function(err, value) {
+      value.should.be.equal('la vispa  altro testo  noo provaaa');
+      done();
+    });
+  });
 
-    it('should accept empty environ',function(done) {
-        substiteVariables(line0, {}, function(err, value) {
-            value.should.be.equal("la vispa  altro testo  noo provaaa");
-            done();
-        });
+  it('should accept empty strings', function(done) {
+    substiteVariables('', {}, function(err, value) {
+      value.should.be.equal('');
+      done();
     });
-    
-    it('should accept empty strings',function(done) {
-        substiteVariables('', {}, function(err, value) {
-            value.should.be.equal('');
-            done();
-        });
-    });
+  });
 
-    it('should substitute a var',function(done) {
-        substiteVariables(line0, { env: { BOH: 1 } }, function(err, value) {
-            value.should.be.equal('la vispa  altro testo 1 noo provaaa');
-            done();
-        });
+  it('should substitute a var', function(done) {
+    substiteVariables(line0, {env: {BOH: 1}}, function(err, value) {
+      value.should.be.equal('la vispa  altro testo 1 noo provaaa');
+      done();
     });
-    
-    it('should substitute multiple vars',function(done) {
-        substiteVariables(line0, { env: env }, function(err, value) {
-            value.should.be.equal('la vispa ciccio altro testo 1 nooaahhh provaaa');
-            done();
-        });
-    });
-    
-    it('should substitute ${BOH}',function(done) {
-        substiteVariables('${BOH}', { env: env }, function(err, value) {
-            value.should.be.equal('1');
-            done();
-        });
-    });
-    
-    it('should substitute $BOH',function(done) {
-        substiteVariables('$BOH', { env: env }, function(err, value) {
-            value.should.be.equal('1');
-            done();
-        });
-    });
- 
-    it('should raise error ${BOH',function(done) {
-        substiteVariables('${BOH', { env: env }, function(err, value) {
-            err.should.be.not.null;
-            done();
-        });
-    });   
+  });
 
-    it('should preserve spaces',function(done) {
-        substiteVariables(' $BOH ', { env: { BOH: 'abc' } }, function(err, value) {
-            value.should.be.equal(' abc ');
-            done();
-        });
+  it('should substitute multiple vars', function(done) {
+    substiteVariables(line0, {env: env}, function(err, value) {
+      value.should.be.equal('la vispa ciccio altro testo 1 nooaahhh provaaa');
+      done();
     });
-    
-    it('should preserve $',function(done) {
-        substiteVariables('$ ${} $', { }, function(err, value) {
-            value.should.be.equal('$ ${} $');
-            done();
-        });
-    });
-    
-    it('should substite $PWD/test',function(done) {
-        substiteVariables('$PWD/test', { env: env }, function(err, value) {
-            value.should.be.equal('/Users/andreax/test');
-            done();
-        });
-    });
-    
-    it('should expand ${PWD:+ciccio}',function(done) {
-        substiteVariables('${PWD:+ciccio}', { env: env }, function(err, value) {
-            value.should.be.equal('ciccio');
-            done();
-        });
-    });
-    
-    it('should expand ${AAA:+bla}',function(done) {
-        substiteVariables('${AAA:+bla}', { env: env }, function(err, value) {
-            value.should.be.equal('');
-            done();
-        });
-    });
-    
-    it('should expand ${PWD:-ciccio}',function(done) {
-        substiteVariables('${PWD:-ciccio}', { env: env }, function(err, value) {
-            value.should.be.equal('/Users/andreax');
-            done();
-        });
-    });
-    
-    it('should expand ${AAA:-bla}',function(done) {
-        substiteVariables('${AAA:-bla}', { env: env }, function(err, value) {
-            value.should.be.equal('bla');
-            done();
-        });
-    });
-    
-    it('should expand ${PWD:#} ${BBB:#}',function(done) {
-        substiteVariables('${PWD:#} ${BBB:#}', { env: env }, function(err, value) {
-            value.should.be.equal('14 0');
-            done();
-        });
-    });
+  });
 
-    it('should expand ${NEWVAR} ${NEWVAR:=newval} ${NEWVAR} ${NEWVAR:=oldval}',function(done) {
-        substiteVariables('${NEWVAR} ${NEWVAR:=newval} ${NEWVAR} ${NEWVAR:=oldval}', { env: env }, function(err, value) {
-            value.should.be.equal(' newval newval newval');
-            done();
-        });
+  it('should substitute ${BOH}', function(done) {
+    substiteVariables('${BOH}', {env: env}, function(err, value) {
+      value.should.be.equal('1');
+      done();
     });
-    
-    it('should expand ${NEWVARx:?} 1 2 3',function(done) {
-        substiteVariables('${NEWVARx:?} 1 2 3', { env: env }, function(err, value) {
-            (err).should.be.equal('NEWVARx: parameter null or not set');
-            done();
-        });
-    });
-    
-    it('should expand ${NEWVARx:?has not been set} 1 2 3',function(done) {
-        substiteVariables('${NEWVARx:?has not been set} 1 2 3', { env: env }, function(err, value) {
-            (err).should.be.equal('NEWVARx: has not been set');
-            done();
-        });
-    });
+  });
 
-    it('should execute functions',function(done) {
-        substiteVariables('${f}', { env: env }, function(err, value) {
-            value.should.be.equal('295');
-            done();
-        });
+  it('should substitute $BOH', function(done) {
+    substiteVariables('$BOH', {env: env}, function(err, value) {
+      value.should.be.equal('1');
+      done();
     });
+  });
 
-    it('should expand $? $$',function(done) {
-        substiteVariables('$? $$', { env: env, specialVars: ['$', '?'] }, function(err, value) {
-            value.should.be.equal('0 12345');
-            done();
-        });
+  it('should raise error ${BOH', function(done) {
+    substiteVariables('${BOH', {env: env}, function(err, value) {
+      err.should.be.not.null;
+      done();
     });
+  });
+
+  it('should preserve spaces', function(done) {
+    substiteVariables(' $BOH ', {env: {BOH: 'abc'}}, function(err, value) {
+      value.should.be.equal(' abc ');
+      done();
+    });
+  });
+
+  it('should preserve $', function(done) {
+    substiteVariables('$ ${} $', {}, function(err, value) {
+      value.should.be.equal('$ ${} $');
+      done();
+    });
+  });
+
+  it('should substite $PWD/test', function(done) {
+    substiteVariables('$PWD/test', {env: env}, function(err, value) {
+      value.should.be.equal('/Users/andreax/test');
+      done();
+    });
+  });
+
+  it('should expand ${PWD:+ciccio}', function(done) {
+    substiteVariables('${PWD:+ciccio}', {env: env}, function(err, value) {
+      value.should.be.equal('ciccio');
+      done();
+    });
+  });
+
+  it('should expand ${AAA:+bla}', function(done) {
+    substiteVariables('${AAA:+bla}', {env: env}, function(err, value) {
+      value.should.be.equal('');
+      done();
+    });
+  });
+
+  it('should expand ${PWD:-ciccio}', function(done) {
+    substiteVariables('${PWD:-ciccio}', {env: env}, function(err, value) {
+      value.should.be.equal('/Users/andreax');
+      done();
+    });
+  });
+
+  it('should expand ${AAA:-bla}', function(done) {
+    substiteVariables('${AAA:-bla}', {env: env}, function(err, value) {
+      value.should.be.equal('bla');
+      done();
+    });
+  });
+
+  it('should expand ${PWD:#} ${BBB:#}', function(done) {
+    substiteVariables('${PWD:#} ${BBB:#}', {env: env}, function(err, value) {
+      value.should.be.equal('14 0');
+      done();
+    });
+  });
+
+  it('should expand ${NEWVAR} ${NEWVAR:=newval} ${NEWVAR} ${NEWVAR:=oldval}', function(done) {
+    substiteVariables(
+      '${NEWVAR} ${NEWVAR:=newval} ${NEWVAR} ${NEWVAR:=oldval}',
+      {env: env},
+      function(err, value) {
+        value.should.be.equal(' newval newval newval');
+        done();
+      }
+    );
+  });
+
+  it('should expand ${NEWVARx:?} 1 2 3', function(done) {
+    substiteVariables('${NEWVARx:?} 1 2 3', {env: env}, function(err, value) {
+      err.should.be.equal('NEWVARx: parameter null or not set');
+      done();
+    });
+  });
+
+  it('should expand ${NEWVARx:?has not been set} 1 2 3', function(done) {
+    substiteVariables(
+      '${NEWVARx:?has not been set} 1 2 3',
+      {env: env},
+      function(err, value) {
+        err.should.be.equal('NEWVARx: has not been set');
+        done();
+      }
+    );
+  });
+
+  it('should execute functions', function(done) {
+    substiteVariables('${f}', {env: env}, function(err, value) {
+      value.should.be.equal('295');
+      done();
+    });
+  });
+
+  it('should expand $? $$', function(done) {
+    substiteVariables('$? $$', {env: env, specialVars: ['$', '?']}, function(err, value) {
+      value.should.be.equal('0 12345');
+      done();
+    });
+  });
 });
